89b2bc488a6e418cc34789344839f0a078cd4dc3
movsb <anhbk@qq.com>
2016-10-14 01:44:35 +0800

    引入界面引擎

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,4 +1,3 @@
-#define  _CRT_SECURE_NO_WARNINGS
 
 #include <cassert>
 
@@ -11,6 +10,8 @@
 
 #define ETW_LOGGER
 #include "etwlogger.h"
+
+#include <taowin/src/tw_taowin.h>
 
 // This GUID defines the event trace class. 	
 // {6E5E5CBC-8ACF-4fa6-98E4-0C63A075323B}
@@ -92,8 +93,90 @@ void __stdcall ProcessEvents(EVENT_TRACE* pEvent)
     std::wcout << log.text << std::endl;
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+class TW : public taowin::window_creator
+{
+public:
+	TW()
+	{}
+
+protected:
+	virtual LPCTSTR get_skin_xml() const override
+	{
+        LPCTSTR json = LR"tw(
+<window title="taowinÑÝÊ¾´°¿Ú" size="500,300">
+    <res>
+        <font name="default" face="Î¢ÈíÑÅºÚ" size="12"/>
+        <font name="1" face="Î¢ÈíÑÅºÚ" size="12"/>
+    </res>
+    <root>
+        <vertical padding="5,5,5,5">
+            <button name="t1" text="È¡Ïû" font="1"/>
+            <listview name="lv" style="singlesel" exstyle="clientedge">  </listview>
+            <horizontal>
+            </horizontal>
+        </vertical>
+    </root>
+</window>
+)tw";
+		return json;
+	}
+
+	virtual LRESULT handle_message(UINT umsg, WPARAM wparam, LPARAM lparam) override
+	{
+		switch(umsg)
+		{
+		case WM_CREATE:
+		{
+			//center();
+            auto lv = _root->find<taowin::listview>(L"lv");
+            lv->insert_column(L"c1", 50, 0);
+            lv->insert_column(L"c2", 50, 1);
+            lv->insert_column(L"c3", 50, 2);
+            lv->insert_item(_T("line1"));
+            lv->insert_item(_T("line2"));
+            lv->insert_item(_T("line3"));
+			return 0;
+		}
+        case WM_CLOSE:
+            if(MessageBox(_hwnd, _T("È·ÈÏ¹Ø±Õ£¿"), nullptr, MB_OKCANCEL) != IDOK) {
+                return 0;
+            }
+		default:
+			break;
+		}
+        return __super::handle_message(umsg, wparam, lparam);
+	}
+
+    virtual LRESULT on_notify(HWND hwnd, taowin::control* pc, int code, NMHDR* hdr) override {
+        if(pc->name() == _T("t1")) {
+            if (code == BN_CLICKED) {
+                return 0;
+            }
+        }
+        return 0;
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 int main()
 {
+    taowin::init();
+
+	try{
+		TW tw1;
+        tw1.create();
+		tw1.show();
+
+        taowin::loop_message();
+	}
+	catch(LPCTSTR){
+
+	}
+
+    taowin::loop_message();
+	
     std::wcout.imbue(std::locale("chs"));
     Controller controller;
     controller.start(L"xxxxxx");

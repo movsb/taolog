73638f64bc0458831c35666cf59e049de1a69813
movsb <anhbk@qq.com>
2016-10-14 13:33:23 +0800

    增加表头 显示/隐藏 设置
    是通过设置宽度为 0 来隐藏的，所以依然可以通过拖动来显示出来

--- src/Source.cpp
+++ src/Source.cpp
@@ -121,6 +121,116 @@ void __stdcall ProcessEvents(EVENT_TRACE* pEvent)
     ::PostMessage(g_hWnd, WM_DO_LOG, 0, LPARAM(log_ui));
 }
 
+//////////////////////////////////////////////////////////////////////////
+
+struct MenuEntry
+{
+    taowin::string name;
+    std::function<void()> onclick;
+};
+
+struct ColumnData
+{
+    struct Column {
+        taowin::string name;
+        bool show;
+        int width;
+    };
+
+    std::vector<Column> cols;
+};
+
+class ColumnSelection : public taowin::window_creator
+{
+private:
+    typedef std::function<void(int i)> OnToggleCallback;
+
+    ColumnData& _data;
+    OnToggleCallback _on_toggle;
+
+public:
+    ColumnSelection(ColumnData& data)
+        : _data(data)
+    {
+
+    }
+
+    void OnToggle(OnToggleCallback fn)
+    {
+        _on_toggle = fn;
+    }
+
+protected:
+    virtual LPCTSTR get_skin_xml() const override
+    {
+        LPCTSTR json = LR"tw(
+<window title="Event columns" size="512,480">
+    <res>
+        <font name="default" face="微软雅黑" size="12"/>
+        <font name="1" face="微软雅黑" size="12"/>
+        <font name="consolas" face="Consolas" size="12"/>
+    </res>
+    <root>
+        <vertical name="container" padding="20,20,20,20">
+
+        </vertical>
+    </root>
+</window>
+)tw";
+        return json;
+    }
+
+    virtual LRESULT handle_message(UINT umsg, WPARAM wparam, LPARAM lparam) override
+    {
+        switch(umsg) {
+        case WM_CREATE:
+        {
+            auto container = _root->find<taowin::vertical>(L"container");
+
+            for(int i = 0; i < (int)_data.cols.size(); i++) {
+                auto check = new taowin::check;
+                std::map<taowin::string, taowin::string> attrs;
+                attrs[_T("text")] = _data.cols[i].name.c_str();
+                attrs[_T("name")] = std::to_wstring(i).c_str();
+                attrs[_T("checked")] = _data.cols[i].show ? _T("true") : _T("false");
+
+                check->create(_hwnd, attrs, _mgr);
+                ::SetWindowLongPtr(check->hwnd(), GWL_USERDATA, LONG(check));
+                container->add(check);
+            }
+
+            taowin::Rect rc {0, 0, 200, (int)_data.cols.size() * 20 + 50};
+            ::AdjustWindowRectEx(&rc, ::GetWindowLongPtr(_hwnd, GWL_STYLE), !!::GetMenu(_hwnd), ::GetWindowLongPtr(_hwnd, GWL_EXSTYLE));
+            ::SetWindowPos(_hwnd, nullptr, 0, 0, rc.width(), rc.height(), SWP_NOMOVE | SWP_NOZORDER);
+
+            return 0;
+        }
+        }
+        return __super::handle_message(umsg, wparam, lparam);
+    }
+
+    virtual LRESULT on_notify(HWND hwnd, taowin::control* pc, int code, NMHDR* hdr)
+    {
+        if(pc->name() >= _T("0") && pc->name() < std::to_wstring(_data.cols.size())) {
+            if(code == BN_CLICKED) {
+                int index = _ttoi(pc->name().c_str());
+                auto& col = _data.cols[index];
+                col.show = !col.show;
+                _on_toggle(index);
+                return 0;
+            }
+        }
+
+        return 0;
+    }
+
+    virtual void on_final_message() override
+    {
+        __super::on_final_message();
+        delete this;
+    }
+};
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 class EventDetail : public taowin::window_creator
 {
@@ -207,6 +317,8 @@ private:
     std::vector<ETWLogger::LogDataUI*> _events;
     struct ItemColor { COLORREF fg; COLORREF bg; };
     std::map<int, ItemColor> _colors;
+    ColumnData _cols;
+    std::vector<MenuEntry> _menus;
 
 public:
 	TW()
@@ -239,22 +351,49 @@ protected:
 		{
             _listview = _root->find<taowin::listview>(L"lv");
 
-            int i = 0;
+            _cols.cols.push_back({L"时间", true, 160 });
+            _cols.cols.push_back({L"进程", true, 50  });
+            _cols.cols.push_back({L"线程", true, 50, });
+            _cols.cols.push_back({L"项目", true, 100,});
+            _cols.cols.push_back({L"文件", true, 200,});
+            _cols.cols.push_back({L"函数", true, 100,});
+            _cols.cols.push_back({L"行号", true, 50, });
+            _cols.cols.push_back({L"日志", true, 300,});
 
-            _listview->insert_column(L"时间", 160, i++);
-            _listview->insert_column(L"进程", 50,  i++);
-            _listview->insert_column(L"线程", 50,  i++);
-            _listview->insert_column(L"项目", 100, i++);
-            _listview->insert_column(L"文件", 200, i++);
-            _listview->insert_column(L"函数", 100, i++);
-            _listview->insert_column(L"行号", 50,  i++);
-            _listview->insert_column(L"日志", 300, i++);
+            for(int i = 0; i < (int)_cols.cols.size(); i++) {
+                auto& col = _cols.cols[i];
+                _listview->insert_column(col.name.c_str(), col.width, i);
+            }
 
             _colors[TRACE_LEVEL_INFORMATION]    = {RGB(  0,   0,   0),  RGB(255, 255, 255)};
             _colors[TRACE_LEVEL_WARNING]        = {RGB(255, 128,   0),  RGB(255, 255, 255)};
             _colors[TRACE_LEVEL_ERROR]          = {RGB(255,   0,   0),  RGB(255, 255, 255)};
             _colors[TRACE_LEVEL_CRITICAL]       = {RGB(255, 255, 255),  RGB(255,   0,   0)};
             _colors[TRACE_LEVEL_VERBOSE]        = {RGB(  0,   0,   0),  RGB(255, 255, 255)};
+
+            MenuEntry menu;
+
+            menu.name = L"表头设置";
+            menu.onclick = [&]() {
+                auto colsel = new ColumnSelection(_cols);
+
+                colsel->OnToggle([&](int i) {
+                    auto& col = _cols.cols[i];
+                    _listview->set_column_width(i, col.show ? col.width : 0);
+                });
+
+                colsel->domodal(this);
+            };
+
+            _menus.push_back(menu);
+
+            HMENU hMenu = ::CreateMenu();
+
+            for(int i = 0; i < (int)_menus.size(); i++) {
+                ::AppendMenu(hMenu, MF_STRING, i, _menus[i].name.c_str());
+            }
+
+            ::SetMenu(_hwnd, hMenu);
 
 			return 0;
 		}
@@ -273,6 +412,15 @@ protected:
 		}
         return __super::handle_message(umsg, wparam, lparam);
 	}
+
+
+    virtual LRESULT on_menu(int id, bool is_accel = false)
+    {
+        if(id < (int)_menus.size()) {
+            _menus[id].onclick();
+        }
+        return 0;
+    }
 
     virtual LRESULT on_notify(HWND hwnd, taowin::control* pc, int code, NMHDR* hdr) override {
         if(pc->name() == _T("lv")) {
